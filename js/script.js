7a38b10761d58fe82a15067ee24a1e96863c4a9d
M Michalczyk <pan.michalczyk@gmail.com>
2018-07-10 13:49:21 +0200

    fix bug3

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 'use strict';
 
 var url = 'https://restcountries.eu/rest/v1/name/';
-var ciuntriesList = document.getElementById('countries');
+var countriesList = document.getElementById('countries');
 
 document.getElementById('search').addEventListener('click', searchCountries);
 
-function searchCountries() {
+function searchCountries {
     var countryName = document.getElementById('country-name').value;
     if (!countryName.length) countryName = 'Poland';
     fetch(url + countryName)
@@ -18,8 +18,8 @@ function searchCountries() {
 function showCountriesList(resp) {
     countriesList.innerHTML = '';
     resp.forEach(function(item) {
-        var liEl = document.createElement('li');
-        liEl.innerText = item.name;
-        countriesList.appendChild(liEl);
+       var liEl = document.createElement('li');
+    	liEl.innerText = 'Country: ' + item.name  +'\nCapital:  '+ item.capital +'\nPopulation:  '+ item.population+'\n\n';
+    	countriesList.appendChild(liEl);
     });
 }
