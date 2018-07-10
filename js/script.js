511edf13ec6f86c507ab0a78dcf089e09bd796cd
M Michalczyk <pan.michalczyk@gmail.com>
2018-07-10 13:42:16 +0200

    fix bug

--- js/script.js
+++ js/script.js
@@ -5,8 +5,8 @@ var ciuntriesList = document.getElementById('countries');
 
 document.getElementById('search').addEventListener('click', searchCountries);
 
-function searchCountries() {
-    var countryName = document.getElementById('country name').value;
+function searchCountries {
+    var countryName = document.getElementById('country-name').value;
     if (!countryName.length) countryName = 'Poland';
     fetch(url + countryName)
         .then(function(resp) {
